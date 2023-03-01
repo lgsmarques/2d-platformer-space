59064d56d464f7a6c8dd0dade2b0095ee4e45127
lgsmarques <luizgsmarques@gmail.com>
2023-03-01 14:24:44 -0300

    add speedRun

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,9 +6,14 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
+    [Header("Movement")]
     public float speed = 10f;
+    public float speedRun = 20f;
     public float forceJump = 20f;
     public Vector2 friction = new Vector2(.1f, 0);
+
+    private float _currentSpeed;
+    private bool _isRunning = false;
 
     private void Update()
     {
@@ -18,13 +23,17 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
+        _isRunning = Input.GetKey(KeyCode.LeftControl);
+        _currentSpeed = _isRunning ? speedRun : speed;
+            
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
 
         if (myRigidbody.velocity.x > 0)
