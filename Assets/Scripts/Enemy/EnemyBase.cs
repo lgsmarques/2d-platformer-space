c548464e0dd98259a0f5653bcc5ce13c4d27cec0
lgsmarques <luizgsmarques@gmail.com>
2023-03-23 15:52:11 -0300

    add enemy and animations

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -6,13 +6,22 @@ public class EnemyBase : MonoBehaviour
 {
     public int damage = 10;
 
+    public Animator animator;
+    public string triggerAttack = "Attack";
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var health = collision.gameObject.GetComponent<HealthBase>();
 
         if (health != null)
         {
-            health.Damage(damage);
+            PlayAttackAnimation();
+            health.Damage(damage);            
         }
+    }
+
+    private void PlayAttackAnimation()
+    {
+        animator.SetTrigger(triggerAttack);
     }
 }
